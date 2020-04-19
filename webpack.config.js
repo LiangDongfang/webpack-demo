d137f1cab64d0961419f1e692fcdf96003e32aac
LiangDongfang <781098813@qq.com>
2020-04-19 14:34:26 +0800

    webpack import css

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
   /*入口配置，默认路径*/
   entry: './src/index.js',/*默认路径*/
   /*出口配置，默认路径*/
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),/*默认路径*/
     filename: '[name].[contenthash].js'
